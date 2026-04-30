96022ae82dd491aefda1c1466483a0894d49c072
kiri1101 <icejordan0@gmail.com>
2026-04-30 11:52:44 +0100

    feat: initiate first commit

---
+++ vite.config.ts
@@ -0,0 +1,48 @@
+import inertia from '@inertiajs/vite';
+import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+import { bunny } from 'laravel-vite-plugin/fonts';
+import { defineConfig } from 'vite';
+import fs from 'fs';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.ts'],
+            refresh: true,
+            fonts: [
+                bunny('Instrument Sans', {
+                    weights: [400, 500, 600],
+                }),
+            ],
+        }),
+        inertia(),
+        tailwindcss(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        wayfinder({
+            formVariants: true,
+        }),
+    ],
+    server: {
+        host: 'student-management-system.test',
+        hmr: {
+            host: 'student-management-system.test', // Required for HMR
+            protocol: 'wss', // Use WebSocket Secure (for SSL)
+        },
+        cors: true, // Explicitly enable CORS
+        https: {
+            // Enable HTTPS for Vite
+            key: fs.readFileSync('D:/laragon/etc/ssl/laragon.key'), // Path to Laragon's key
+            cert: fs.readFileSync('D:/laragon/etc/ssl/laragon.crt'), // Path to Laragon's cert
+        },
+    },
+});
